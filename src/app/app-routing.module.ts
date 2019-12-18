72ae712204802edb2e5385b2cab5d2d204bb486d
vikas <vkumar@loginsoft.com>
2019-12-18 17:08:15 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CorejavaComponent } from './corejava/corejava.component';
+import { JdbcComponent } from './jdbc/jdbc.component';
+import { ServletComponent } from './servlet/servlet.component';
+import { JspComponent } from './jsp/jsp.component';
+import { HibernateComponent } from './hibernate/hibernate.component';
+import { SpringComponent } from './spring/spring.component';
+import { SpringbootComponent } from './springboot/springboot.component';
+import { InterviewquestionComponent } from './interviewquestion/interviewquestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+ {path: 'home', component:HomeComponent},
+ {path: 'corejava', component:CorejavaComponent},
+ {path: 'jdbc', component:JdbcComponent},
+ {path: 'servlet', component:ServletComponent},
+ {path: 'jsp', component:JspComponent},
+ {path: 'hibernate', component:HibernateComponent},
+ {path: 'spring', component:SpringComponent},
+ {path: 'springboot', component:SpringbootComponent},
+ {path: 'interviewquestion', component: InterviewquestionComponent}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/interviewquestion/interviewquestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interviewquestion',
+  templateUrl: './interviewquestion.component.html',
+  styleUrls: ['./interviewquestion.component.css']
+})
+export class InterviewquestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
